de6971659fe58af056ef61166f58e655a7d1cd93
LAMMINAHO\Petri <lammpe77@gmail.com>
2017-10-27 20:12:34 +0300

    tenserboard graph

--- graph.py
+++ graph.py
@@ -8,13 +8,6 @@ res = node1 + node2
 # print(res) # prints: Tensor("add:0", shape=(), dtype=float32) not res value
 
 sess = tf.Session()
+file_writer = tf.summary.FileWriter('graph', sess.graph) # makes the graph
 print(sess.run(res))
 sess.close()  # close the session
-
-# or you can use with sentence
-
-with tf.Session() as sess:
-    output = sess.run(res)
-    print("with sentence", output)
-    # you don't need to close session if you use 'with'
-
